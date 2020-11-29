b81c00d358bd71b0cd7c72f1ca0ee814dee6764f
Nyamtsetseg <nyamtsetsegidertsog@gmail.com>
2020-11-29 16:23:50 +0800

    lab 4 nemlee

---
+++ components/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Header.css"
+const Header = () => {
+
+    return (
+        <div>
+            <div className="head">
+                <div>
+                    <img src='components/header/pro.png' alt="" className='pro'/>
+                </div>
+            </div>
+        </div>
+    )
+} 
+export default Header;
---
+++ p2.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import Head from './components/header/Header.jsx'
+import States from './components/states/States';
+
+
+ReactDOM.render(
+  <div>
+    {/* <Head/> */}
+    <States/>
+  </div>,
+  document.getElementById('reactapp'),
+);
